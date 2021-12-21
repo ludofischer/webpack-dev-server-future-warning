8d4617991a2af0974521dee400fcb5dc56ad2338
Ludovico Fischer <livrerie@gmail.com>
2021-12-22 00:14:19 +0100

    Remove module output

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 
    experiments: {
-     outputModule: true,
      futureDefaults: true,
    },
 
-   plugins: [new HtmlWebpackPlugin({scriptLoading: 'module'})]
+   plugins: [new HtmlWebpackPlugin()]
 }
